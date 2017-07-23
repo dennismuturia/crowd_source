ffbd21a2537dac901f64954de337de4c5587f500
dennismuturia <dmuturia2922@gmail.com>
2017-07-23 15:06:34 +0300

    Added sweet alert

--- app/components/company-detail.js
+++ app/components/company-detail.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+      destroyReview(review) {
+        review.destroyRecord();
+      }
+    }
 });
--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import swal from 'sweetalert';
 
 export default Ember.Component.extend({
+  actions:{
+    delete(review) {
+      if (swal('Are you sure you want to delete this review?')) {
+        this.sendAction('destroyReview', review);
+      }
+  }
+}
 });
