16bc3ca61bb4c245790eff65c6dd5d7bc45f7398
dennismuturia <dmuturia2922@gmail.com>
2017-07-23 15:09:43 +0300

    Added font awesome

--- app/helpers/company-star.js
+++ app/helpers/company-star.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
-export function companyStar(params/*, hash*/) {
-  return params;
+export function companyStar(params) {
+  var company = params[0];
+
+    if(company.get('reviews').get('length') >= 5) {
+      return Ember.String.htmlSafe('<i class="fa fa-star"></span>');
+    }
 }
 
 export default Ember.Helper.helper(companyStar);
